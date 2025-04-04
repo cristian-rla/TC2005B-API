6d989326027f4d44abf4d85cd701bfc6109fef04
cristian-rla <Cristianrluquea@hotmail.com>
2025-04-03 18:59:28 -0600

    Completed product get method segmented in modules

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,1 +1,8 @@
-const product = require('express')
+import express from 'express'
+import handler from "../handlers/product"
+
+const router = express.Router();
+
+router.get("/", handler.getAll);
+
+export default router;
