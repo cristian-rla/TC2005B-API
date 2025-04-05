87ed86fe6777fa88eafa86d3a095fcbbeb4919c8
cristian-rla <Cristianrluquea@hotmail.com>
2025-04-05 17:51:39 -0600

    added authRouter to server.ts

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import productRouter from "./src/routes/product"
 import negotiationRouter from "./src/routes/negotiation"
 import clientRouter from "./src/routes/client"
+import authRouter from "./src/routes/auth"
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use(cors());
 app.use("/api/productoServicio", productRouter);
 app.use("/api/negociacion", negotiationRouter);
 app.use("/api/cliente", clientRouter);
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
