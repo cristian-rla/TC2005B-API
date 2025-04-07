db18f438c01553e329fdaf0684f18bff16aa708a
cristian-rla <Cristianrluquea@hotmail.com>
2025-04-06 23:43:52 -0600

    auth changed. it works

--- src/handlers/auth.ts
+++ src/handlers/auth.ts
@@ -9,7 +9,7 @@ class AuthHandler{
     async logIn(req:Request, res:Response, next:NextFunction){
         try{
             if (!await authController.verifyEmail(req.body)){
-                throw (new Error("La cuenta ya existe"))
+                throw (new Error("La cuenta no existe"))
             }
             res.status(200).json({message: "Se ha iniciado sesión"})
         } catch(error:unknown){
